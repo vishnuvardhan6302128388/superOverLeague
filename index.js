007fe5d6f3e5692fed4e63b47eec2a4b568e6c9c
vishnuvardhan6302128388 <158791693+vishnuvardhan6302128388@users.noreply.github.com>
2024-03-14 12:18:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className='bg-container'>
+    <h1 className='heading'>Super Over League</h1>
+    <div className='card-container'>
+      <img
+        className='images'
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+      />
+      <img
+        className='images'
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
